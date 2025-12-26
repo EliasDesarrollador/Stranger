50a421ee93315406cad41c987bb4fe223f90f0b6
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-12-26 19:48:15 -0300

    Archivo de configuracion general del juego

--- config.py
+++ config.py
@@ -1,10 +1,14 @@
 
 # config.py 
-#Archivo de configuracion del juego 
+
+#Archivo de configuracion general del juego 
+# Tamano de la ventana 
 ANCHO = 800
 ALTO = 600
+# FPS = cuadros por segundo 
 FPS = 60
 
+# Colores en formato RGB 
 NEGRO = (0, 0, 0)
 ROJO  = (200, 50,50)
 VERDE = (50, 200 ,50 )
